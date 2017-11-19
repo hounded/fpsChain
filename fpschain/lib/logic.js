be522a90febfddc9af58cee5e8254313cc758a08
whiteja5 <james.whiteman@effem.com>
2017-11-20 12:06:54 +1300

    working on the blockchain

--- fpschain/lib/logic.js
+++ fpschain/lib/logic.js
@@ -9,15 +9,15 @@
  * @transaction
  */
 function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('mars.ap.wgi.wgiplant.fpschain.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
-        });
+    // var assetRegistry;
+    // var id = changeAssetValue.relatedAsset.assetId;
+    // return getAssetRegistry('mars.ap.wgi.wgiplant.fpschain.SampleAsset')
+    //     .then(function(ar) {
+    //         assetRegistry = ar;
+    //         return assetRegistry.get(id);
+    //     })
+    //     .then(function(asset) {
+    //         asset.value = changeAssetValue.newValue;
+    //         return assetRegistry.update(asset);
+    //     });
 }
